b2797295811efac56b82f767bd5b4fd26a6729c9
hsu41234 <125813718+hsu41234@users.noreply.github.com>
2024-06-16 14:34:11 +0800

    Add files via upload

---
+++ week3.php
@@ -0,0 +1,37 @@
+<html>
+<head>
+<meta charset="utf-10">
+</head>
+<body>
+<p><center><h1>許仕毅</h1>
+<img src="https://www.hoganbakery.com.tw/wp-content/uploads/2023/02/%E7%94%A2%E5%93%81%E7%85%A7-%E5%AE%98%E7%B6%B2-%E5%90%90%E5%8F%B8%E7%B3%BB%E5%88%97-%E7%9A%87%E5%86%A0%E7%89%9B%E5%A5%B6%E5%90%90%E5%8F%B8-1%E5%8E%9A%E7%89%87.jpg" width="200" height="300" alt="照片"/></center></p>
+<hr color="red"width="80%" align ="left"/>
+<p><h2>簡介</h2>
+<ul>
+    <li><b>生日：</b>2004/06/25
+    <li><b>信箱：</b>jeff105193@gmail.com</li>
+
+</ul></p>
+<hr color="pink"width="801%" align ="left"/>
+<p><h2>自我介紹</h2>
+
+<font color="blue">我吐司只吃切邊的 但我放沒切的。</font></p>
+<p><center><table border="">
+<thread>
+<h2>工作經歷</h2>
+
+<table border="">
+    <caption>工作經歷表</caption>
+    <thread>
+        <tr>
+            <th>工作職稱</th>
+            <th>工作期間</th>
+            <th>工作內容</th>
+        </tr>
+    </thread>
+    <tbody>
+        <tr>
+            <td>工廠作業員</td>
+            <td>2022年7月~2022年8月</td>
+            <td>操作機器</td>
+        </tr>
